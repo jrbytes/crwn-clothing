70a6a5ae70b23ea3efc86e02d3bd21d232c4fb45
JrBytes <itamazor@gmail.com>
2020-03-21 17:08:35 -0300

    updated our application App component, sign-in component and sign-up component to use hooks

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 
@@ -9,24 +9,19 @@ import CollectionOverviewContainer from
 import CollectionPageContainer from
 '../collection/collection.container'
 
-class ShopPage extends React.Component {
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+  useEffect(() => {
     fetchCollectionsStart()
-  }
+  }, [fetchCollectionsStart])
 
-  render() {
-    const { match } = this.props
-    
-    return (
-      <div className='shop-page'>
-        <Route exact path={`${match.path}`} 
-        component={CollectionOverviewContainer} />
-        <Route path={`${match.path}/:collectionId`} 
-        component={CollectionPageContainer} />
-      </div>
-    )
-  }
+  return (
+    <div className='shop-page'>
+      <Route exact path={`${match.path}`} 
+      component={CollectionOverviewContainer} />
+      <Route path={`${match.path}/:collectionId`} 
+      component={CollectionPageContainer} />
+    </div>
+  )
 }
 
 const mapDispatchToProps = dispatch => ({
